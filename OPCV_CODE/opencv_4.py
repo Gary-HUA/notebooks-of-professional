ada0e71650a6e5333664f57adaecb3a2c0081ddb
Gary-HUA <64634997+Gary-HUA@users.noreply.github.com>
2021-02-13 07:37:22 +0530

    reconstructed

--- OPCV_CODE/opencv_4.py
+++
@@ -1,7 +0,0 @@
-import cv2 as cv
-src = cv.imread("E:/python_OpenCV lib/lena.jpg")
-res = src[20:400, 50:220]  # H W POSITION 把图像按照期望进行切割截取
-cv.imshow("result", res)
-cv.imwrite("E:/python_OpenCV lib/lena_cutting.jpg", res)
-cv.waitKey(0)
-cv.destroyAllWindows()
